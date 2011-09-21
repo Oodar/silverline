38e58e54b59d89807313520276e41bb33c5f5410
Matthew <matthew.hewitson@btinternet.com>
2011-09-21 18:09:17 +0100

    Changed layout to allow more/less button to float outside quote div

--- quoteMore.js
+++ quoteMore.js
@@ -5,19 +5,30 @@ jQuery(document).ready(function() {
 	
 	$(".more").live( 'click', function() {
 		console.log("clicked on more");
-		var pHeight = $(this).parents('p').height();
-		var pPadding = $(this).parents('p').css('padding-bottom');
+
+		var pHeight = $(this).parents('div:first').children('.quote-content').children('p').height();
+		var pPadBot = $(this).parents('div:first').children('.quote-content').children('p').css('padding-bottom');
+		var pPadTop = $(this).parents('div:first').children('.quote-content').children('p').css('padding-top');
+
+		var pPadding = parseInt(pPadBot) + parseInt(pPadTop);
+
+		console.log("pHeight: " + pHeight + " pPadding: " + pPadding);
+
 		var finalHeight = parseInt(pHeight) + parseInt(pPadding);
 
 		$(this).parents('div:first').animate({height: finalHeight}, 1000);
+		$(this).parents('div:first').children('.quote-content').animate({height: finalHeight}, 1000);
 
 		$(this).removeClass("more").addClass("less");
+
 	});
 
 	
 	$(".less").live( 'click', function() {
 		console.log("clicked on less");
+
 		$(this).parents('div:first').animate({height: startHeight}, 1000);
+		$(this).parents('div:first').children('.quote-content').animate({height: startHeight}, 1000);
 
 		$(this).removeClass("less").addClass("more");
 	});
